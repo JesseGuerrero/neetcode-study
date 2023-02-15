6ae253bc08d5c926d63b3b55a4127e827dca35e4
Jesse Guerrero <jesseguerrero1991@gmail.com>
2023-02-14 20:44:58 -0600

    update

--- LinkedListUtils.py
+++ LinkedListUtils.py
@@ -1,33 +1,41 @@
+'''
+Making a new variable will create a brand new node, pass by value.
+Going out of scope of a function will reset the node, pass by value.
+'''
+
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-def reverseList(head: ListNode) -> ListNode:
-    prev, curr = None, head
+def printListNode(node : ListNode):
+    while node != None:
+        print(node.val, end=", ")
+        node = node.next
 
-    while curr:
-        temp = curr.next
-        curr.next = prev
-        prev = curr
-        curr = temp
+
+def reverseList(head: ListNode) -> ListNode:
+    prev = None
+    while head:
+        next_node = head.next
+        head.next = prev
+        prev = head
+        head = next_node
     return prev
 
 def createListNode(nums : list) -> ListNode:
     listnode = ListNode(nums[0], ListNode())
     temp = listnode.next
-    for n in nums[1:]:
+    for i, n in enumerate(nums[1:]):
         temp.val = n
-        temp.next = ListNode(n)
-        temp = temp.next
+        if i < len(nums)-2:
+            temp.next = ListNode(n)
+            temp = temp.next
     return listnode
 
-def printListNode(listnode : ListNode):
-    while listnode.next != None:
-        print(listnode.val, end=", ")
-        listnode = listnode.next
-
-listnode = createListNode([1, 2, 3])
-printListNode(listnode)
-# print(reverseList())
+node = createListNode([1, 2, 3])
+# print(f'{node.val} {node.next.val} {node.next.next.val} {node.next.next.next}')
+# printListNode(node)
+printListNode(reverseList(node))
